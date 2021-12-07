efc8e548351b09e2586841317e75168e42e80246
Pedro López Mareque <Pedro.lopez.mareque@gmail.com>
2021-12-07 19:46:13 +0100

    feat: refactor day two 2016

--- src/2016/day_02/index.ts
+++ src/2016/day_02/index.ts
@@ -15,63 +15,71 @@ const keypad2 = [
 export function solvePart1(input: string[]): number {
   let x = 1;
   let y = 1;
-  let code = '';
 
-  for (let i = 0; i < input.length; i++) {
-    const moves = input[i].split('');
-    for (let j = 0; j < moves.length; j++) {
-      const key = moves[j];
-      switch (key) {
-        case 'U':
-          y = y === 0 ? 0 : y - 1;
-          break;
-        case 'D':
-          y = y === 2 ? 2 : y + 1;
-          break;
-        case 'L':
-          x = x === 0 ? 0 : x - 1;
-          break;
-        case 'R':
-          x = x === 2 ? 2 : x + 1;
-          break;
-        default:
-          break;
-      }
-    }
-    code += `${keypad[y][x]}`;
-  }
+  const key = input.reduce((acc, current) => {
+    current.split('').reduce(
+      (acc, current) => {
+        switch (current) {
+          case 'U':
+            y = y === 0 ? 0 : y - 1;
+            break;
+          case 'D':
+            y = y === 2 ? 2 : y + 1;
+            break;
+          case 'L':
+            x = x === 0 ? 0 : x - 1;
+            break;
+          case 'R':
+            x = x === 2 ? 2 : x + 1;
+            break;
+          default:
+            break;
+        }
+        acc.x = x;
+        acc.y = y;
+        return acc;
+      },
+      { x: 0, y: 0 },
+    );
+    acc += `${keypad[y][x]}`;
+    return acc;
+  }, '');
 
-  return Number(code);
+  return Number(key);
 }
 
 export function solvePart2(input: string[]): string {
   let x = 0;
   let y = 2;
-  let code = '';
 
-  for (let i = 0; i < input.length; i++) {
-    const moves = input[i].split('');
-    for (let j = 0; j < moves.length; j++) {
-      const key = moves[j];
-      switch (key) {
-        case 'U':
-          y = y === 0 || keypad2[x][y - 1] === 0 ? y : y - 1;
-          break;
-        case 'D':
-          y = y === 4 || keypad2[x][y + 1] === 0 ? y : y + 1;
-          break;
-        case 'L':
-          x = x === 0 || keypad2[x - 1][y] === 0 ? x : x - 1;
-          break;
-        case 'R':
-          x = x === 4 || keypad2[x + 1][y] === 0 ? x : x + 1;
-          break;
-        default:
-          break;
-      }
-    }
-    code += `${keypad2[y][x]}`;
-  }
+  const key = input.reduce((acc, current) => {
+    current.split('').reduce(
+      (acc, current) => {
+        switch (current) {
+          case 'U':
+            y = y === 0 || keypad2[x][y - 1] === 0 ? y : y - 1;
+            break;
+          case 'D':
+            y = y === 4 || keypad2[x][y + 1] === 0 ? y : y + 1;
+            break;
+          case 'L':
+            x = x === 0 || keypad2[x - 1][y] === 0 ? x : x - 1;
+            break;
+          case 'R':
+            x = x === 4 || keypad2[x + 1][y] === 0 ? x : x + 1;
+            break;
+          default:
+            break;
+        }
+        acc.x = x;
+        acc.y = y;
+        return acc;
+      },
+      { x: 0, y: 0 },
+    );
+    acc += `${keypad2[y][x]}`;
+    return acc;
+  }, '');
 
-  return code;
+  return key;
 }
