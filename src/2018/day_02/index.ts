222e5777d418c8a8d9ecb9edf20ed5030ed0ad47
Pedro López Mareque <Pedro.lopez.mareque@gmail.com>
2021-12-11 20:04:34 +0100

    feat: refactor day two 2018

--- src/2018/day_02/index.ts
+++ src/2018/day_02/index.ts
@@ -26,5 +26,22 @@ function countCharacters(char: string, word: string): number {
 }
 
 export function solvePart2(input: string[]): string {
-  return '';
+  let str = '';
+  for (let i = 0; i < input.length; i++) {
+    for (let y = 1; y < input.length - 1; y++) {
+      const diff = findDiff(input[i], input[y]);
+      if (diff.length == 1) {
+        str = input[y].replace(diff, '');
+        break;
+      }
+    }
+  }
+
+  return str;
+}
+
+function findDiff(c1: string, c2: string): string {
+  return c2
+    .split('')
+    .reduce((acc, val, i) => (val != c1.charAt(i) ? (acc += val) : acc), '');
 }
