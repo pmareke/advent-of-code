529edcc7e49aac4f1ca34d3c57cbc847c6f4c11a
Pedro López Mareque <Pedro.lopez.mareque@gmail.com>
2021-12-23 06:55:10 +0100

    chore: fix typo

--- __test__/2016/day_02.spec.ts
+++ __test__/2016/day_02.spec.ts
@@ -2,7 +2,7 @@ import { solvePart1, solvePart2 } from '../../src/2016/day_02';
 import { readAllLines } from '../../utils';
 import { TestCase } from '../../utils/types';
 
-describe('Day One', () => {
+describe('Day Two', () => {
   const example = ['ULL', 'RRDDD', 'LURDL', 'UUUUD'];
 
   const real = readAllLines('2016', '02');
