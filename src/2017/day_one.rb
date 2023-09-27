1e38e29a034a5f559f4567717f548536585eb3f3
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2023-09-27 20:32:07 +0200

    fix rubocop

--- src/2017/day_one.rb
+++ src/2017/day_one.rb
@@ -2,16 +2,16 @@
 
 class DayOne2017
   def self.part_one(numbers)
-    calculate_catch(numbers, step: 1)
+    calculate_catch(numbers, 1)
   end
 
   def self.part_two(numbers)
     step = numbers.size / 2
-    calculate_catch(numbers, step:)
+    calculate_catch(numbers, step)
   end
 
   class << self
-    def calculate_catch(numbers, step:)
+    def calculate_catch(numbers, step)
       digits = []
       numbers.each_with_index do |number, index|
         next_index = (index + step) % numbers.size
