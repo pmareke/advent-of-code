638d921a18b32683d92be20e357b0130e2ab4593
Pedro López Mareque <Pedro.lopez.mareque@gmail.com>
2023-10-17 17:19:46 +0200

    feat: solve day 5 from 2022

---
+++ src/2022/day_five.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+class DayFive2022
+  def self.part_one(lines)
+    buckets = head
+    lines.each do |line|
+      times, from, to = parse_line line
+      move = buckets[from - 1].pop(times).reverse
+      buckets[to - 1].push(*move)
+    end
+    buckets.map(&:last).join
+  end
+
+  def self.part_two(lines)
+    buckets = head
+    lines.each do |line|
+      times, from, to = parse_line line
+      move = buckets[from - 1].pop(times)
+      buckets[to - 1].push(*move)
+    end
+    buckets.map(&:last).join
+  end
+
+  class << self
+    def head
+      [
+        %w[W D G B H R V],
+        %w[J N G C R F],
+        %w[L S F H D N J],
+        %w[J D S V],
+        %w[S H D R Q W N V],
+        %w[P G H C M],
+        %w[F J B G L Z H C],
+        %w[S J R],
+        %w[L G S R B N V M]
+      ]
+    end
+
+    def parse_line(line)
+      regex = /move (\d+) from (\d+) to (\d+)/
+      line.scan(regex).flatten.map(&:to_i)
+    end
+  end
+end
---
+++ test/2022/test_day_five.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+require_relative "../../src/2022/day_five"
+
+class TestDayFive2022 < Minitest::Test
+  def setup
+    @lines = File.open("inputs/2022/day_five.in").readlines
+  end
+
+  def test_part_one
+    result = DayFive2022.part_one(@lines)
+
+    assert_equal "JRVNHHCSJ", result
+  end
+
+  def test_part_two
+    result = DayFive2022.part_two(@lines)
+
+    assert_equal "GNFBSBJLH", result
+  end
+end
