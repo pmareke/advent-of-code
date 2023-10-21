6727e9a6282c00376bad41ae7b4a6b150d32f5d2
Pedro López Mareque <Pedro.lopez.mareque@gmail.com>
2023-10-21 10:52:19 +0200

    feat: solve day 5 from 2017

--- test/test_suite.rb
+++ test/test_suite.rb
@@ -18,6 +18,7 @@ require_relative "2017/test_day_one"
 require_relative "2017/test_day_two"
 require_relative "2017/test_day_three"
 require_relative "2017/test_day_four"
+require_relative "2017/test_day_five"
 
 require_relative "2018/test_day_one"
 require_relative "2018/test_day_two"
