d268d2e76ed6c0f45b1df0d7067bd535d9dd90a3
Pedro López Mareque <Pedro.lopez.mareque@gmail.com>
2023-10-26 11:08:45 +0200

    feat: refactor day 5 from 2019

--- src/2019/day_five.rb
+++ src/2019/day_five.rb
@@ -17,19 +17,11 @@ class DayFive2019
         operation, modes = parse_operation(numbers[pointer])
         case operation
         when 1
-          x = numbers[pointer + 1]
-          p1 = modes[0] ? numbers[x] : x
-          y = numbers[pointer + 2]
-          p2 = modes[1] ? numbers[y] : y
-          out = numbers[pointer + 3]
+          p1, p2, out = operation_with_three_parameters(numbers, pointer, modes)
           numbers[out] = p1 + p2
           pointer += 4
         when 2
-          x = numbers[pointer + 1]
-          p1 = modes[0] ? numbers[x] : x
-          y = numbers[pointer + 2]
-          p2 = modes[1] ? numbers[y] : y
-          out = numbers[pointer + 3]
+          p1, p2, out = operation_with_three_parameters(numbers, pointer, modes)
           numbers[out] = p1 * p2
           pointer += 4
         when 3
@@ -41,32 +33,18 @@ class DayFive2019
           output = numbers[current]
           pointer += 2
         when 5
-          x = numbers[pointer + 1]
-          p1 = modes[0] ? numbers[x] : x
-          y = numbers[pointer + 2]
-          p2 = modes[1] ? numbers[y] : y
+          p1, p2 = operation_with_two_parameters(numbers, pointer, modes)
           pointer = p1.zero? ? pointer + 3 : p2
         when 6
-          x = numbers[pointer + 1]
-          p1 = modes[0] ? numbers[x] : x
-          y = numbers[pointer + 2]
-          p2 = modes[1] ? numbers[y] : y
+          p1, p2 = operation_with_two_parameters(numbers, pointer, modes)
           pointer = p1.zero? ? p2 : pointer + 3
         when 7
-          x = numbers[pointer + 1]
-          p1 = modes[0] ? numbers[x] : x
-          y = numbers[pointer + 2]
-          p2 = modes[1] ? numbers[y] : y
-          z = numbers[pointer + 3]
-          numbers[z] = p1 < p2 ? 1 : 0
+          p1, p2, out = operation_with_three_parameters(numbers, pointer, modes)
+          numbers[out] = p1 < p2 ? 1 : 0
           pointer += 4
         when 8
-          x = numbers[pointer + 1]
-          p1 = modes[0] ? numbers[x] : x
-          y = numbers[pointer + 2]
-          p2 = modes[1] ? numbers[y] : y
-          z = numbers[pointer + 3]
-          numbers[z] = p1 == p2 ? 1 : 0
+          p1, p2, out = operation_with_three_parameters(numbers, pointer, modes)
+          numbers[out] = p1 == p2 ? 1 : 0
           pointer += 4
         when 99
           return output
@@ -79,6 +57,23 @@ class DayFive2019
       opcode = str.last(2).join.to_i
       modes = str[0..-3].map(&:to_i).map(&:zero?).reverse
       [opcode, modes]
+    end
+
+    def operation_with_three_parameters(numbers, pointer, modes)
+      x = numbers[pointer + 1]
+      p1 = modes[0] ? numbers[x] : x
+      y = numbers[pointer + 2]
+      p2 = modes[1] ? numbers[y] : y
+      out = numbers[pointer + 3]
+      [p1, p2, out]
+    end
+
+    def operation_with_two_parameters(numbers, pointer, modes)
+      x = numbers[pointer + 1]
+      p1 = modes[0] ? numbers[x] : x
+      y = numbers[pointer + 2]
+      p2 = modes[1] ? numbers[y] : y
+      [p1, p2]
     end
   end
 end
