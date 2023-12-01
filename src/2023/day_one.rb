f0dcad048f8717f54b77f7a2b36fb21cad251ab1
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2023-12-01 08:39:41 +0100

    feat: refactor day one 2023

--- src/2023/day_one.rb
+++ src/2023/day_one.rb
@@ -14,8 +14,8 @@ class DayOne2023
     numbers = %w[one two three four five six seven eight nine]
     lines.sum do |line|
       digits = line.strip.scan(/(?=(one|two|three|four|five|six|seven|eight|nine|\d))/).flatten
-      first = numbers.include?(digits.first) ? (numbers.index(digits.first) + 1) : digits.first.to_i
-      last = numbers.include?(digits.last) ? (numbers.index(digits.last) + 1) : digits.last.to_i
+      first = numbers.index(digits.first)&.next || digits.first
+      last = numbers.index(digits.last)&.next || digits.last
       "#{first}#{last}".to_i
     end
   end
