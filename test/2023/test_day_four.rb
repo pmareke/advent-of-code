9dd798134e387eb925737fbb32cb08571cf202b9
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2023-12-03 09:23:12 +0100

    feat: prepare day four 2023

---
+++ test/2023/test_day_four.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require_relative "../../src/2023/day_four"
+require "minitest/autorun"
+
+class TestDayFour2023 < Minitest::Test
+  def setup
+    lines = File.open("inputs/2023/day_four.in")
+    @input = lines.readlines.map(&:strip)
+  end
+
+  def test_part_one
+    result = DayFour2023.part_one(@input)
+
+    assert_equal 0, result
+  end
+
+  def test_part_two
+    result = DayFour2023.part_two(@input)
+
+    assert_equal 0, result
+  end
+end
