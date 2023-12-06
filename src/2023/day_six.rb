486fa52399684aaa9eb88e2f0aac2df294fc7b55
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2023-12-06 08:48:51 +0100

    feat: refactor day six 2023

--- src/2023/day_six.rb
+++ src/2023/day_six.rb
@@ -8,7 +8,7 @@ class DaySix2023
 
   def self.part_two(lines)
     race = lines.reduce([]) { |acc, line| acc << line.scan(/(?:\d+)/).join.to_i }
-    play(*race)
+    play_with_formula(*race)
   end
 
   class << self
@@ -18,6 +18,14 @@ class DaySix2023
         distance = movement_time * millimeters_per_millisecond
         record < distance
       end
+    end
+
+    def play_with_formula(time, distance)
+      # [Diophantine equation](https://en.wikipedia.org/wiki/Diophantine_equation)
+      discriminant = Math.sqrt((time ** 2) - (4 * distance))
+      x = (time + discriminant) / 2
+      y = (time - discriminant) / 2
+      x.floor - y.ceil + 1
     end
   end
 end
