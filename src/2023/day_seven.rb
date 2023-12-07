6d01b49d2c8bb80c6ee483ad8d382e6bc0061cc3
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2023-12-07 11:35:18 +0100

    feat: extract logic to the hand class for day seven 2023

--- src/2023/day_seven.rb
+++ src/2023/day_seven.rb
@@ -1,78 +1,24 @@
 # frozen_string_literal: true
 
-module HandType
-  FIVE_OF_A_KIND = 7
-  FOUR_OF_A_KIND = 6
-  FULL_HOUSE = 5
-  THREE_OF_A_KIND = 4
-  TWO_PAIR = 3
-  ONE_PAIR = 2
-  HIGH_CARD = 1
-end
+require_relative "hand"
 
 class DaySeven2023
   def self.part_one(lines)
-    cards_strength = %w[A K Q J T 9 8 7 6 5 4 3 2]
-    solve(lines, cards_strength)
+    solve(lines)
   end
 
   def self.part_two(lines)
-    cards_strength = %w[A K Q T 9 8 7 6 5 4 3 2 J]
-    solve(lines, cards_strength, part_two: true)
+    solve(lines, part_two: true)
   end
 
   class << self
-    def solve(lines, cards_strength, part_two: false)
+    def solve(lines, part_two: false)
       hands = lines.reduce([]) do |acc, line|
         cards, bid = line.split
-        type = part_two ? find_hand_type_part_two(cards.chars) : find_hand_type(cards.chars)
-        acc << [type, cards, bid.to_i]
+        acc << Hand.new(cards.chars, bid, part_two:)
       end
 
-      sorted_hands = hands.sort { |h1, h2| compare(h1, h2, cards_strength) }
-      sorted_hands.each_with_index.reduce([]) { |acc, (hand, index)| acc << (hand[2] * index.next) }.reduce(&:+)
-    end
-
-    def find_hand_type(cards)
-      group_by_letter = cards.tally
-      repetitions = group_by_letter.values.sort
-      {
-        [5] => HandType::FIVE_OF_A_KIND,
-        [1, 4] => HandType::FOUR_OF_A_KIND,
-        [2, 3] => HandType::FULL_HOUSE,
-        [1, 1, 3] => HandType::THREE_OF_A_KIND,
-        [1, 2, 2] => HandType::TWO_PAIR,
-        [1, 1, 1, 2] => HandType::ONE_PAIR,
-        [1, 1, 1, 1, 1] => HandType::HIGH_CARD,
-      }[repetitions]
-    end
-
-    def find_hand_type_part_two(cards)
-      return find_hand_type(cards) unless cards.include? "J"
-
-      {
-        HandType::FIVE_OF_A_KIND => HandType::FIVE_OF_A_KIND,
-        HandType::FOUR_OF_A_KIND => HandType::FIVE_OF_A_KIND,
-        HandType::FULL_HOUSE => HandType::FIVE_OF_A_KIND,
-        HandType::THREE_OF_A_KIND => HandType::FOUR_OF_A_KIND,
-        HandType::TWO_PAIR => cards.count("J") == 2 ? HandType::FOUR_OF_A_KIND : HandType::FULL_HOUSE,
-        HandType::ONE_PAIR => HandType::THREE_OF_A_KIND,
-        HandType::HIGH_CARD => HandType::ONE_PAIR,
-      }[find_hand_type(cards)]
-    end
-
-    def compare(first_hand, second_hand, cards_strength)
-      return 1 if first_hand.first > second_hand.first
-
-      return -1 if first_hand.first < second_hand.first
-
-      first_hand[1].chars.each_with_index do |letter, index|
-        next if letter == second_hand[1][index]
-
-        return 1 if cards_strength.find_index(letter) < cards_strength.find_index(second_hand[1][index])
-
-        return -1
-      end
+      hands.sort.each_with_index.reduce([]) { |acc, (hand, index)| acc << (hand.bid * index.next) }.reduce(&:+)
     end
   end
 end
---
+++ src/2023/hand.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+module HandType
+  FIVE_OF_A_KIND = 7
+  FOUR_OF_A_KIND = 6
+  FULL_HOUSE = 5
+  THREE_OF_A_KIND = 4
+  TWO_PAIR = 3
+  ONE_PAIR = 2
+  HIGH_CARD = 1
+end
+
+class Hand
+  attr_reader :cards, :bid, :type
+
+  def initialize(cards, bid, part_two: false)
+    @cards = cards
+    @bid = bid.to_i
+    @type = part_two ? find_hand_type_part_two : find_hand_type
+
+    @cards_strength = part_two ? %w[A K Q T 9 8 7 6 5 4 3 2 J] : %w[A K Q J T 9 8 7 6 5 4 3 2]
+  end
+
+  def find_hand_type
+    group_by_letter = @cards.tally
+    repetitions = group_by_letter.values.sort
+    {
+      [5] => HandType::FIVE_OF_A_KIND,
+      [1, 4] => HandType::FOUR_OF_A_KIND,
+      [2, 3] => HandType::FULL_HOUSE,
+      [1, 1, 3] => HandType::THREE_OF_A_KIND,
+      [1, 2, 2] => HandType::TWO_PAIR,
+      [1, 1, 1, 2] => HandType::ONE_PAIR,
+      [1, 1, 1, 1, 1] => HandType::HIGH_CARD,
+    }[repetitions]
+  end
+
+  def find_hand_type_part_two
+    return find_hand_type unless @cards.include? "J"
+
+    {
+      HandType::FIVE_OF_A_KIND => HandType::FIVE_OF_A_KIND,
+      HandType::FOUR_OF_A_KIND => HandType::FIVE_OF_A_KIND,
+      HandType::FULL_HOUSE => HandType::FIVE_OF_A_KIND,
+      HandType::THREE_OF_A_KIND => HandType::FOUR_OF_A_KIND,
+      HandType::TWO_PAIR => @cards.count("J") == 2 ? HandType::FOUR_OF_A_KIND : HandType::FULL_HOUSE,
+      HandType::ONE_PAIR => HandType::THREE_OF_A_KIND,
+      HandType::HIGH_CARD => HandType::ONE_PAIR,
+    }[find_hand_type]
+  end
+
+  def <=>(other)
+    return 1 if @type > other.type
+
+    return -1 if @type < other.type
+
+    @cards.each_with_index do |letter, index|
+      next if letter == other.cards[index]
+
+      return 1 if @cards_strength.find_index(letter) < @cards_strength.find_index(other.cards[index])
+
+      return -1
+    end
+  end
+end
