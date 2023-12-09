0f05cc76b0a9127fa7437d70e13e46b817a6053d
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2023-12-09 10:22:20 +0100

    feat: refactor day nine 2023

--- src/2023/day_nine.rb
+++ src/2023/day_nine.rb
@@ -4,26 +4,27 @@ class DayNine2023
   def self.part_one(lines)
     lines.sum do |line|
       digits = line.split.map(&:to_i)
-      numbers = prediction([digits])
-      numbers.reverse.each_cons(2) do |(x, y)|
-        y.push(y.last + x.last)
-      end
-      numbers.first.last
+      calculate_prediction(digits).last
     end
   end
 
   def self.part_two(lines)
     lines.sum do |line|
       digits = line.split.map(&:to_i)
-      numbers = prediction([digits])
-      numbers.reverse.each_cons(2) do |(x, y)|
-        y.unshift(y.first - x.first)
-      end
-      numbers.first.first
+      calculate_prediction(digits).first
     end
   end
 
   class << self
+    def calculate_prediction(digits)
+      matrix = prediction([digits])
+      matrix.reverse.each_cons(2) do |(x, y)|
+        y.unshift(y.first - x.first)
+        y.push(y.last + x.last)
+      end
+      matrix.first
+    end
+
     def prediction(numbers)
       return numbers if numbers.last.all?(&:zero?)
 
