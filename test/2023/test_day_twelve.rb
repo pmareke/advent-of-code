28c7ac6b752caa72974c7b083fb1261c6d428c1c
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2023-12-11 11:30:33 +0100

    feat: prepare day twelve 2023

---
+++ test/2023/test_day_twelve.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require_relative "../../src/2023/day_twelve"
+require "minitest/autorun"
+
+class TestDayTwelve2023 < Minitest::Test
+  def setup
+    file = "inputs/2023/day_twelve.in"
+    @lines = File.readlines(file).map(&:strip)
+  end
+
+  def test_part_one
+    result = DayTwelve2023.part_one(@lines)
+
+    assert_equal 0, result
+  end
+
+  def test_part_two
+    result = DayTwelve2023.part_two(@lines)
+
+    assert_equal 0, result
+  end
+end
